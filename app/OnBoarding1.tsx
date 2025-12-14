46cbed546a9810e7a43038f0a8fe41f016a0b2c9
Darshan <darshanchoudhary2007@gmail.com>
2025-12-14 21:11:57 +0530

    first onboarding screen done

--- app/OnBoarding1.tsx
+++ app/OnBoarding1.tsx
@@ -1,36 +1,88 @@
-import { View, Text, Image, StyleSheet } from 'react-native';
-import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const OnBoarding1 = () => {
   return (
-    <SafeAreaView>
-        <View>
-            <Text style={styles.skip}>Skip</Text>
-        </View>
-        <View>
-            <Image source={require("../assets/onboarding/ob1.png")} style={styles.image}/>
-            <Text>Skilled Professionals You Can Trust</Text>
-            <Text>Trained Professionals for home and daily services</Text>
-        </View>
-        <View style={styles.button}><Text style={styles.buttonText}>Continue</Text></View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ alignItems: "flex-end" }}>
+        <Text style={styles.skip}>Skip</Text>
+      </View>
+      <View style={{ alignItems: "center", marginTop: 50,gap:20 }}>
+        <Image
+          source={require("../assets/onboarding/ob1.png")}
+          style={styles.image}
+        />
+        <Text style={{ fontSize: 26, textAlign: "center" }}>
+          Skilled Professionals You Can Trust
+        </Text>
+        <Text style={{ textAlign: "center", color: "#757575" }}>
+          Trained Professionals for home and daily services
+        </Text>
+      </View>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Continue</Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          gap: 5,
+          alignSelf: "center",
+        //   position: "absolute",
+        //   bottom: 40,
+        //   left: "50%",
+        //   right: "50%",
+        }}
+      >
+        <View style={styles.currentDot}></View>
+        <View style={styles.dot}></View>
+        <View style={styles.dot}></View>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    skip:{
-        color:'grey'
-    },
-    image:{
-        height:100,
-        width:100
-    },
-    button:{
-        backgroundColor:'#4560F4'
-    },
-    buttonText:{
-        color : "white"
-    }
-})
-export default OnBoarding1
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 20,
+    justifyContent:'space-between'
+  },
+  skip: {
+    color: "grey",
+    fontSize:20
+  },
+  image: {
+    height: 300,
+    width: 300,
+  },
+  button: {
+    backgroundColor: "#4560F4",
+    height: 54,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 25,
+    // position: "absolute",
+    // bottom: 60,
+    // left: 20,
+    // right: 20,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 22,
+  },
+  dot: {
+    height: 5,
+    width: 5,
+    backgroundColor: "#B5B5B5",
+    borderRadius: "50%",
+  },
+  currentDot: {
+    height: 5,
+    width: 30,
+    backgroundColor: "#008CFF",
+    borderRadius: 50,
+  },
+});
+export default OnBoarding1;

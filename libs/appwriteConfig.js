47475137764a17c44e7a1bc61fd5cb0c400ade1b
Shreyash Shirke <shreyashshirkes2leo@gmail.com>
2026-02-14 17:00:36 +0530

    staging

---
+++ libs/appwriteConfig.js
@@ -0,0 +1,8 @@
+// import { Client, Account } from "appwrite";
+// import { EXPO_PUBLIC_APPWRITE_ENDPOINT, EXPO_PUBLIC_APPWRITE_PROJECT_ID } from "../appwrite";
+// const client = new Client()
+// client.setEndpoint(EXPO_PUBLIC_APPWRITE_ENDPOINT)
+// client.setProject(EXPO_PUBLIC_APPWRITE_PROJECT_ID)
+// const account = new Account(client)
+
+// export { account }
